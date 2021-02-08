a01d5799f580a2152adffd61ce56bfbadcc3c73a
reesh19 <rodriguezr235@gmail.com>
2021-02-08 10:57:48 -0800

    day 1

---
+++ lambdata/__init__.py
@@ -0,0 +1,8 @@
+import pandas
+import numpy
+
+def df_cleanser(df):
+    """cleans pd.DataFrame"""
+    # TODO - Implement
+    pass
+    
---
+++ lambdata/example_module.py
@@ -0,0 +1,10 @@
+"""Lambdata - A collection of Data Science Helper functions"""
+
+import pandas as pd
+import numpy as np
+
+def df_cleanser(df):
+    
+
+"""A module in Lamdata"""
+FAVORITE_ANIMALS = ['dog','cat','husky','wolf']
